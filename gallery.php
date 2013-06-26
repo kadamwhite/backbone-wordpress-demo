445739bd5369ba7fd9b571618dd77e45b55cee7f
K.Adam White <kadamwhite@gmail.com>
2013-06-26 01:26:41 -0400

    Initial, PHP-heavy solution to the artwork gallery UI
    This approach favors rendering all of the content using WordPress, and only leveraging JS/jQuery to show and hide elements when handling user interaction

--- gallery.php
+++ gallery.php
@@ -17,8 +17,59 @@ if ( is_front_page() ) :
 	if($artwork_items->have_posts()) :
 ?>
 <div class="artwork-header-gallery">
+	<?php
+		while( $artwork_items->have_posts() ) : $artwork_items->the_post();
+			$permalink_title = esc_attr( sprintf( __( 'Permalink to %s', 'artwork' ), the_title_attribute( 'echo=0' ) ) );
+	?>
+	<div class="artwork-featured" style="display: none;">
+		<a class="artwork-image"
+			 href="<?php the_permalink(); ?>"
+			 title="<?php echo $permalink_title; ?>"
+			 rel="bookmark">
+			<?php the_post_thumbnail( 'large' ); ?>
+		</a>
+		<div class="artwork-information">
+			<h3>
+				<a href="<?php the_permalink(); ?>"
+					 title="<?php echo $permalink_title; ?>"
+					 rel="bookmark">
+					<?php the_title(); ?>
+				</a>
+			</h3>
+			<?php
+				// Using `get_the_content` to opt out of the post type plugin's custom formatting in `the_content` filter
+				echo wpautop( get_the_content() );
+
+				// Get taxonomies
+				$dimensions = get_the_term_list(
+					$post->ID,
+					'ag_artwork_dimensions',
+					'Dimensions: ',
+					', ',
+					''
+				);
+
+				$media = get_the_term_list(
+					$post->ID,
+					'ag_artwork_media',
+					'Media: ',
+					', ',
+					''
+				);
+			?>
+			<div class="entry-meta">
+				<p><?php the_author(); ?></p>
+				<p><?php echo $dimensions; ?></p>
+				<p><?php echo $media; ?></p>
+			</div> <!-- .entry-meta -->
+		</div> <!-- .artwork-information -->
+	</div> <!-- .artwork-featured -->
+	<?php endwhile; ?>
 	<div class="artwork-thumbnails">
-		<?php while( $artwork_items->have_posts() ) : $artwork_items->the_post(); ?>
+		<?php
+			rewind_posts(); // Second verse, same as the first
+			while( $artwork_items->have_posts() ) : $artwork_items->the_post();
+		?>
 
 		<a href="<?php the_permalink(); ?>"
 			 title="<?php echo esc_attr(sprintf( __( '%s', 'artwork' ), the_title_attribute( 'echo=0' ) ) ); ?>">
