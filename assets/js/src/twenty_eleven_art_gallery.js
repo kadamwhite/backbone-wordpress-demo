a734f18b39903f554f410fa3bea393d02c535482
K.Adam White <kadamwhite@gmail.com>
2013-06-26 09:55:34 -0400

    Convert all remaining jQuery interactions to Backbone
    Continuing where we left off, we further organize our code by moving the event handling for the Thumbnails container and the conditional hiding of the header banner into two new Backbone views. Now we have no explicit jQuery DOM traversal anywhere in our application, and all view updates are driven by the Featured Artwork model's data. Application initialization consists of defining the models and views, and Backbone takes it from there.

--- assets/js/src/twenty_eleven_art_gallery.js
+++ assets/js/src/twenty_eleven_art_gallery.js
@@ -10,38 +10,27 @@
 (function( window, $, models, views, undefined ) {
 	'use strict';
 
-	var $thumbnails, artwork, featuredArtwork;
+	var artwork, featuredArtwork, banner, thumbnails;
 
-	// Define our Backbone model & view instances
+	// Define our Backbone model
 	artwork = new models.Artwork();
+
+	// Define the views for our little gallery. Note that these all use
+	// the same model: `thumbnails` receives click events and sets the data,
+	// `featuredArtwork` renders the featured artwork area, and `banner`
+	// just hides the top banner the first time the gallery opens
+	thumbnails = new views.Thumbnails({
+		el: '.artwork-thumbnails',
+		model: artwork
+	});
+
 	featuredArtwork = new views.FeaturedArtwork({
 		model: artwork
 	});
 
-	// Don't need `$(document).ready` since this script is loaded at page bottom
-	$thumbnails = $('.artwork-thumbnails');
-
-	// Catch any clicks on the image thumbnails
-	$thumbnails.on('click', 'a', function( evt ) {
-		var data;
-
-		evt.preventDefault();
-
-		// Get the featured image data from the link we clicked
-		data = $( this ).data();
-
-		// If we clicked the same image that's showing, do nothing
-		if ( artwork.id === data.id ) {
-			return;
-		}
-
-		if ( !artwork.id ) {
-			// If the gallery is not yet open, hide the banner
-			$('#branding').children('a').slideUp();
-			$thumbnails.addClass('open'); // For styling only
-		}
-
-		// Set the data for the model, which will trigger a view `.render()`
-		artwork.set( data );
+	banner = new views.Banner({
+		el: '#branding > a',
+		model: artwork
 	});
+
 })( this, jQuery, this.artgallery.models, this.artgallery.views );
