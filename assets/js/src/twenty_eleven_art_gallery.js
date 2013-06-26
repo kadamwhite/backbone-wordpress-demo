99e390644170b6cff37b8ce54f55048c93ecf929
K.Adam White <kadamwhite@gmail.com>
2013-06-26 01:41:50 -0400

    Move the rendering of featured images to jQuery using `data-` attributes
    This simplifies both the PHP *and* the JavaScript, somewhat:
    
    * In PHP, we don't have to iterate through the list twice, and
    * In JavaScript, we don't have to do as much DOM lookup.
    
    Unfortunately the `render()` method puts HTML into our JS, where it is less than welcome!

--- assets/js/src/twenty_eleven_art_gallery.js
+++ assets/js/src/twenty_eleven_art_gallery.js
@@ -9,41 +9,60 @@
 (function( window, $, undefined ) {
 	'use strict';
 
-	$(document).ready(function() {
-		var $headerGallery, $thumbnails, $featuredArtwork;
+	var $headerGallery, $thumbnails, $featuredArtwork, render;
 
-		$headerGallery = $('.artwork-header-gallery');
-		$thumbnails = $headerGallery.find('.artwork-thumbnails');
-		$featuredArtwork = $headerGallery.find('.artwork-featured');
+	render = function( data ) {
+		// Concatenate the HTML and the data into a JavaScript string
+		return [
+			'<a class="artwork-image" href="', data.url, '" title="', data.permalink, '" rel="bookmark">',
+				data.image,
+			'</a>',
+			'<div class="artwork-information">',
+				'<h3>',
+					'<a href="', data.url, '" title="', data.permalink, '" rel="bookmark">',
+						data.title,
+					'</a>',
+				'</h3>',
+				data.content,
+				'<div class="entry-meta">',
+					'<p>', data.author, '</p>',
+					'<p>', data.dimensions, '</p>',
+					'<p>', data.media, '</p>',
+				'</div>',
+			'</div>'
+		].join('');
+	};
 
-		// Catch any clicks on the image thumbnails
-		$thumbnails.on('click', 'a', function( evt ) {
-			var index, $selectedItem, $visibleItems;
+	// Don't need `$(document).ready` since this script is loaded at page bottom
+	$headerGallery = $('.artwork-header-gallery');
+	$thumbnails = $headerGallery.find('.artwork-thumbnails');
+	$featuredArtwork = $headerGallery.find('.artwork-featured');
 
-			evt.preventDefault();
+	// Catch any clicks on the image thumbnails
+	$thumbnails.on('click', 'a', function( evt ) {
+		var data;
 
-			// Figure out which thumbnail we clicked
-			index = $( this ).index();
+		evt.preventDefault();
 
-			// Match that index to featured image div
-			$selectedItem = $featuredArtwork.eq( index );
-			$visibleItems = $featuredArtwork.filter(':visible');
+		// Get the featured image data from the link we clicked
+		data = $( this ).data();
 
-			// If we clicked the same image that's showing, do nothing
-			if ( $visibleItems.index() === index ) {
-				return;
-			}
+		// If we clicked the same image that's showing, do nothing
+		if ( data.id === $featuredArtwork.data('id') ) {
+			return;
+		}
 
-			// If the gallery is open, just switch the image
-			if ( $visibleItems.length ) {
-				$visibleItems.hide();
-				$selectedItem.show();
-			} else {
-				// If the gallery is not yet open, slide it open and hide the banner
-				$('#branding').children('a').slideUp();
-				$selectedItem.slideDown();
-				$thumbnails.addClass('open'); // For styling only
-			}
-		});
+		// If the gallery is open, just switch the image
+		if ( $featuredArtwork.is(':visible') ) {
+			$featuredArtwork
+				.html( render( data ) )
+				// Set the ID so we can do that convenient comparison up above
+				.data( 'id', data.id );
+		} else {
+			// If the gallery is not yet open, slide it open and hide the banner
+			$('#branding').children('a').slideUp();
+			$featuredArtwork.html( render( data ) ).slideDown();
+			$thumbnails.addClass('open'); // For styling only
+		}
 	});
 })( this, jQuery );
